afe6b07f7b2d7834bf5fe6532aa591591b07abc9
Shipra Upadhyay <117964967+Shipra53@users.noreply.github.com>
2025-02-27 17:49:09 +0530

    Factorial

---
+++ fact.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class fact {
+    public static int factorial(int n){
+        int f=1;
+        for(int i=n;i>=1;i--){
+            f=f*i;
+        }
+        return f;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=sc.nextInt();
+        int res=factorial(n);
+        System.out.println("Factorial of "+n+" is: "+res);
+    }
+}
